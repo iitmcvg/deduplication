4729a67f7eb3a608eb913797ab49b6d1973e0891
Kaushik-Raghavan <koushrk@gmail.com>
2015-12-28 15:25:27 +0530

    Changed folder structure, added query_database.cpp

---
+++ query_database.cpp
@@ -0,0 +1,15 @@
+// Format: ./a.out p/d <image path> <database file path>#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <cstdio>
+#include <pHash.h>
+#include <pHash-config.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+int 
